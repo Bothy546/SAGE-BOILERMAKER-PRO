79c42181461b6278f8959914b6f835da7cb6189c
Bothy546 <chikamburemelvin@gmail.com>
2026-08-20 17:06:59 +0200

    Add MainActivity for the application

---
+++ app/src/main/java/com/sage/boilermakerpro/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sage.boilermakerpro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.sage.boilermakerpro.navigation.SageApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    SageApp()
+                }
+            }
+        }
+    }
+}
