b2d65d93c682b5935b3abb6e529481df1d1407ae
Bothy546 <chikamburemelvin@gmail.com>
2026-08-30 18:44:22 +0200

    Add AcademyProgress.kt for tracking lessons and scores

---
+++ app/src/main/java/com/sage/boilermakerpro/data/AcademyProgress.kt
@@ -0,0 +1,6 @@
+package com.sage.boilermakerpro.data
+
+import androidx.compose.runtime.mutableStateMapOf
+
+val completedLessons = mutableStateMapOf<String, Boolean>()
+val quizScores = mutableStateMapOf<String, Int>()
